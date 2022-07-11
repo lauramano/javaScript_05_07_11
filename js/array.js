14b7ac7ae538a88e8744dbf99e71bef47e671107
laurapras <83669900+laurapras@users.noreply.github.com>
2022-07-11 14:06:36 +0300

    arrayUzd padaryta

---
+++ js/array.js
@@ -0,0 +1,20 @@
+const u1 = "Arturas";
+const u2 = "Bernelis";
+const u3 = "Pagalys";
+
+// array yra masyvas
+const names = ["Arturas", "Bernelis", "Pagalys" ];
+console.log(names[1]);
+
+const numbers = [1, 5, 9, 12];
+console.log(numbers);
+// ivairiu tipu masyvas
+
+const mixed = [45, "James", true, null, [5, 6]];
+console.log(mixed);
+console.log(mixed[4][1]);
+
+const colors = ['red', 'green', 'blue'];
+console.log(color[0]);
+console.log(color[1]);
+console.log(color[2]);
---
+++ js/arrayUzd.js
@@ -0,0 +1,13 @@
+console.log('Sekmingo darbo :)');
+//             0         1           2       3       4       5       6
+const pav = ['apple', 'sister', 'hammer', 'sun', 'table', 'sky', 'lake'];
+//             0     1       2   3       4   5       6   7
+const pag = ['in', 'with', 'on', 'my', 'is', 'a', 'the', ''];
+
+// 2 hammer is on the table
+const table =console.log(`${pav[2]} ${pag[4]} ${pag[2]} ${pag[6]} ${pav[4]} `);
+// 3 sun is in the sky
+const sky = console.log(`${pav[3]} ${pag[4]} ${pag[0]} ${pag[6]} ${pav[5]}`);
+// 4 apple is with my sister
+const sister = console.log(`${pav[0]} ${pag[4]} ${pag[1]} ${pag[3]} ${pav[1]}`);
+
