14ef5f6977d58e1d8d6f140570740c645f7c6e29
laurapras <83669900+laurapras@users.noreply.github.com>
2022-07-16 19:00:22 +0300

    metod masyvai

---
+++ js/arrMeth.js
@@ -0,0 +1,65 @@
+'use strict';
+console.log('Metodu panaudijimas masyvuose');
+//  Metodai reindeksuojantys masyvus (keiciasi eiles nr.)
+
+const colors = ['green', 'blue', 'red'];
+console.log('orginalus masyvas', colors);
+
+// array.push() prideda i masyvo gala ir modifikuoja masyva
+
+const pushResult = colors.push('violet');
+console.log('pushResult atspausdina reiksme', pushResult);
+console.log('modifikuotas masyvas su pridetu elementu', colors);
+
+// array.unshift('nauja reiksme') - prideda reiksme i prieki
+
+const unshiftResult = colors.unshift('white');
+console.log('unshiftResult atspausdina reiksme', unshiftResult);
+console.log('modifikuotas masyvas su pridetu elementu', colors);
+
+//  ===========            ===================
+
+//  ==============               =================
+// array.shift - nuima reiksme nuo priekio  ir ja pasiima
+
+const shiftResult = colors.shift();
+console.log('unshiftResult atspausdina reiksme', shiftResult);
+console.log('modifikuotas masyvas su nuimtu elementu', colors);
+
+// array.pop() - nuima reiksme nuo galo. ir ja pasiima
+
+const popResult = colors.pop();
+console.log('popResult atspausdina reiksme', popResult);
+console.log('modifikuotas masyvas su nuimtu elementu', colors);
+
+//  ============ Nemodifikuojantis masyvo =======================
+
+// array.slice(start, finish) (iskeliau funkcija kad paimtu ilgiausia masyva)
+let sliceResult = colors.slice();
+sliceResult = colors.slice(2, 5); // nuo 3 elemento iki 5
+sliceResult = colors.slice(-1); // Paskutine reiksme (-2 dvi paskutines reiksmes)
+console.log('funcijos slice rezultatas', sliceResult);
+console.log('modifikuotas masyvas su nuimtu elementu', colors);
+
+const manPatinka = ['lietus', 'sniegas', 'saule'];
+const maNepatinka = ['krusa', 'speigas', 'debesys'];
+
+// array.concat() sujungia du masyvus
+let manResult = manPatinka.concat(maNepatinka);
+const manResultResult = manResult.concat('Apelsinas');
+console.log(manResultResult);
+console.log(manPatinka);
+console.log(maNepatinka);
+console.log('array.concat masyvo rezultatas', manResult);
+
+// array.splice() (dievazi daro viska.... pasalinti ir prideti vienu metu pirmas prideda antras kiek reikia pasalinti)
+
+const masyvas = ['asara', 'lietus', 'voras', 'pavasaris']
+const masyvasResult = masyvas.splice(0, 0, "Lemon", "Kiwi");
+console.log('Pradinis masyvas', masyvas );
+console.log('Modifikuotas masyvas', masyvasResult );
+
+// pasitikrinimas ar tai masyvas ir ka grazina reiksmes
+
+console.log('const colors tipas ', typeof colors);
+console.log('Ar tai masyvas(grazins true oaba folse)', Array.isArray(colors));
--- js/array.js
+++ js/array.js
@@ -1,9 +1,11 @@
+'use strict';
 const u1 = "Arturas";
 const u2 = "Bernelis";
 const u3 = "Pagalys";
 
 // array yra masyvas
 const names = ["Arturas", "Bernelis", "Pagalys" ];
+console.log(names);
 console.log(names[1]);
 
 let numbers = [1, 5, 9, 12];
@@ -15,12 +17,19 @@ console.log(mixed);
 console.log(mixed[4][1]);
 
 const colors = ['red', 'green', 'blue'];
-console.log(colors[0]);
-console.log(colors[1]);
-console.log(colors[2]);
+// console.log(colors[0]);
+// console.log(colors[1]);
+// console.log(colors[2]);
+
+for( let i=0;  i<3; i++){
+    console.log(colors[i]);
+}
 // pasirasyti for cikliuka :)
 numbers = [46, 1, 4, 78, 9, 5, 1, 4, 5, 5, 6];
 console.log(numbers);
 console.log(numbers.length);
 // atspausdinti paskutini nari
 console.log(numbers[numbers.length - 1]);   
+for(let i = 0; i<numbers.length; i++){
+    console.log(`skaicius ${numbers[i]}`)
+}
